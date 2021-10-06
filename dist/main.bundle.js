cbeb204447b348a1e10b104dc2d536a402f20d4e
Hector <vv96rca@gmail.com>
2021-10-06 12:26:28 +0530

    added screenshot

---
+++ dist/main.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+let addBtn = document.getElementById('button')
+let name =  document.getElementById('name')
+let score =  document.getElementById('score')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0EsNkMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9sZWFkZXJib2FyZC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJsZXQgYWRkQnRuID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ2J1dHRvbicpXHJcbmxldCBuYW1lID0gIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCduYW1lJylcclxubGV0IHNjb3JlID0gIGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdzY29yZScpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,3 @@
+let addBtn = document.getElementById('button')
+let name =  document.getElementById('name')
+let score =  document.getElementById('score')
